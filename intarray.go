4b6e3db1c3a0b2c95eb699c4fc11032eec0cd918
sakipiyo <86043482+sakipiyo@users.noreply.github.com>
2023-01-23 14:56:59 +0900

    Add files via upload

---
+++ intarray.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	intvals := [5]int{98, 125, 232, 147, 486}
+	fmt.Printf("1番目の要素は%d\n", intvals[1])
+	fmt.Printf("0番目の要素は%d\n", intvals[0])
+	fmt.Printf("4番目の要素は%d\n", intvals[4])
+	fmt.Printf("2番目と3番目の要素を足すと%d\n", intvals[1]+intvals[2])
+}
